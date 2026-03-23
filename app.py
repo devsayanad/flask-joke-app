b79dbd77a7eb363dafdb2e60eaff8738f1d3893d
devsayanad <sayanad151@gmail.com>
2026-03-23 07:42:42 +0545

    updated

--- app.py
+++ app.py
@@ -52,5 +52,8 @@ def saved():
 
     return render_template("saved.html", jokes=jokes)
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
